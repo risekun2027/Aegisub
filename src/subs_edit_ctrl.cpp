e4f7b28ab7b4d8a0d9e4179083613b914c54dfc9
risekun2027 <risekun2027@outlook.com>
2025-11-17 02:08:30 +0100

    Implement OnChar handler for text direction support
    Added OnChar event handler to manage RTL and LTR text input.

--- src/subs_edit_ctrl.cpp
+++ src/subs_edit_ctrl.cpp
@@ -123,6 +123,7 @@ SubsTextEditCtrl::SubsTextEditCtrl(wxWindow* parent, wxSize wsize, long style, a
 	using std::bind;
 
 	Bind(wxEVT_CHAR_HOOK, &SubsTextEditCtrl::OnKeyDown, this);
+	Bind(wxEVT_CHAR, &SubsTextEditCtrl::OnChar, this);
 
 	Bind(wxEVT_MENU, bind(&SubsTextEditCtrl::Cut, this), EDIT_MENU_CUT);
 	Bind(wxEVT_MENU, bind(&SubsTextEditCtrl::Copy, this), EDIT_MENU_COPY);
@@ -205,8 +206,27 @@ void SubsTextEditCtrl::OnLoseFocus(wxFocusEvent &event) {
 	event.Skip();
 }
 
+void SubsTextEditCtrl::OnChar(wxKeyEvent &event) {
+	event.Skip();
+
+	// TODO upgrade system to support both RTL and LTR by fixing data
+	if (GetTextRaw().length() == 0) {
+		if (IsCharRTL(event.GetUnicodeKey()))
+		{
+			SetTextTo(RTL_MARK);
+			SetLayoutDirection(wxLayout_RightToLeft);
+			SetSelection(GetSelectionStart() + 5, GetSelectionStart() + 5);
+		}
+		else
+		{
+			SetLayoutDirection(wxLayout_LeftToRight);
+		}
+	}
+
+
+}
+
 void SubsTextEditCtrl::OnKeyDown(wxKeyEvent &event) {
-	if (osx::ime::process_key_event(this, event)) return;
 	event.Skip();
 
 	// Workaround for wxSTC eating tabs.
